c568f81d6a708f1962ca151060d9a0d8acdc9b3e
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-14 21:02:03 -0400

    Added the logic that will check for connection

--- app/src/main/java/com/raywenderlich/reposearch/MainActivity.java
+++ app/src/main/java/com/raywenderlich/reposearch/MainActivity.java
@@ -24,15 +24,18 @@ package com.raywenderlich.reposearch;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -51,8 +54,31 @@ public class MainActivity extends AppCompatActivity {
       if (savedInstanceState != null) {
         return;
       }
-      showListFragment(new ArrayList<Repository>());
+
+      if (isNetworkConnected()) {
+          mProgressDialog = new ProgressDialog(this);
+          mProgressDialog.setMessage("Please wait...");
+          mProgressDialog.setCancelable(false);
+          mProgressDialog.show();
+
+          startDownload();
+      } else {
+          new AlertDialog.Builder(this)
+                  .setTitle("No Internet Connection")
+                  .setMessage("It looks like your internet connection is off. Please turn it on and try again")
+                  .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener(){
+                      public void onClick(DialogInterface dialog, int which){
+                      }
+                  }).setIcon(android.R.drawable.ic_dialog_alert).show();
+      }
     }
+  }
+
+  private boolean isNetworkConnected() {
+      ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+      NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+      return networkInfo != null && networkInfo.isConnected();
   }
 
   private void showListFragment(ArrayList<Repository> repositories) {
@@ -76,12 +102,5 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void startDownload() {
-  }
-
-  private boolean isNetworkConnected(){
-    ConnectivityManager connMgr = (ConnectivityManager)
-            getSystemService(Context.CONNECTIVITY_SERVICE);
-    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-    return networkInfo != null && networkInfo.isConnected();
   }
 }
